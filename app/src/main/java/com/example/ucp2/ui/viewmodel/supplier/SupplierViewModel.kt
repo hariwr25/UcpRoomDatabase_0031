50f17bd00de4e154ab1548fb2f4ee1235ae39a6b
hariwr <hariwr25@gmail.com>
2024-12-25 02:20:16 +0700

    Menambahkan Data Class dan fix error

--- app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/SupplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/SupplierViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.ucp2.data.entity.Supplier
 import com.example.ucp2.repository.supplier.RepositorySup
 
 class SupplierViewModel(private val repositorySup: RepositorySup) : ViewModel() {
@@ -24,32 +25,63 @@ class SupplierViewModel(private val repositorySup: RepositorySup) : ViewModel() 
         uiSplState = uiSplState.copy(isEntrySplValid = errorSplState)
         return errorSplState.isSplValid()
     }
-}
 
-suspend fun saveDataSupplier(): Boolean {
-    val currentSplEvent = uiSplState.supplierEvent
+    suspend fun saveDataSupplier(): Boolean {
+        val currentSplEvent = uiSplState.supplierEvent
 
-    return if (validateSplFields()) {
-        try {
-            repositorySpl.insertSupplier(currentSplEvent.toSupplierEntity())
-            uiSplState = uiSplState.copy(
-                snackBarMessage = "Data berhasil disimpan ke dalam sistem.",
-                supplierEvent = SupplierEvent(),
-                isEntrySplValid = FormErrorSplState()
-            )
-            true
-        } catch (e: Exception) {
+        return if (validateSplFields()) {
+            try {
+                repositorySup.insertSupplier(currentSplEvent.toSupplierEntity())
+                uiSplState = uiSplState.copy(
+                    snackBarMessage = "Data berhasil disimpan ke dalam sistem.",
+                    supplierEvent = SupplierEvent(),
+                    isEntrySplValid = FormErrorSplState()
+                )
+                true
+            } catch (e: Exception) {
+                uiSplState =
+                    uiSplState.copy(snackBarMessage = "Terjadi kesalahan saat menyimpan data supplier.")
+                false
+            }
+        } else {
             uiSplState =
-                uiSplState.copy(snackBarMessage = "Terjadi kesalahan saat menyimpan data supplier.")
+                uiSplState.copy(snackBarMessage = "Data input tidak valid. Mohon periksa kembali.")
             false
         }
-    } else {
-        uiSplState =
-            uiSplState.copy(snackBarMessage = "Data input tidak valid. Mohon periksa kembali.")
-        false
+    }
+
+    fun resetSnackBarSplMessage() {
+        uiSplState = uiSplState.copy(snackBarMessage = null)
     }
 }
 
-fun resetSnackBarSplMessage() {
-    uiSplState = uiSplState.copy(snackBarMessage = null)
+data class splUIState(
+    val supplierEvent: SupplierEvent = SupplierEvent(),
+    val isEntrySplValid: FormErrorSplState = FormErrorSplState(),
+    val snackBarMessage: String? = null
+)
+
+data class FormErrorSplState(
+    val nama: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null
+) {
+    fun isSplValid(): Boolean {
+        return nama == null &&
+                kontak == null && alamat == null
+    }
 }
+
+fun SupplierEvent.toSupplierEntity(): Supplier = Supplier(
+    id = id,
+    nama = nama,
+    kontak = kontak,
+    alamat = alamat
+)
+
+data class SupplierEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val kontak: String = "",
+    val alamat: String = ""
+)
