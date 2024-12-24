6620dab33af489d37456e7e0aff9237996395b72
hariwr <hariwr25@gmail.com>
2024-12-25 02:04:17 +0700

    Tambahkan SupplierHomeViewModel untuk mengelola data supplier dengan StateFlow

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/SupplierHomeViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.ucp2.ui.viewmodel.supplier
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.repository.supplier.RepositorySup
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class SupplierHomeViewModel (
+    private val repositorySpl: RepositorySup
+) : ViewModel() {
+
+    val homeUiStateSpl: StateFlow<HomeUIStateSpl> = repositorySpl.getAllSupplier()
+        .filterNotNull()
+        .map {
+            HomeUIStateSpl (
+                listSpl = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(HomeUIStateSpl(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUIStateSpl(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUIStateSpl(
+                isLoading = true
+            )
+        )
+}
