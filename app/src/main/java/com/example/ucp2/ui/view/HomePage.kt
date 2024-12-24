14e34c3ee54dd400a8efca6d42b9776903ee602d
hariwr <hariwr25@gmail.com>
2024-12-25 02:32:26 +0700

    Tambahkan BodySection

--- app/src/main/java/com/example/ucp2/ui/view/HomePage.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HomePage.kt
@@ -1,13 +1,35 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.R
 
 @Preview(showBackground = true)
 @Composable
@@ -29,3 +51,73 @@ fun Homepage(
     ) {
     }
 }
+
+@Composable
+fun HeaderSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(bottomEnd = 48.dp))
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(0xFF6200EA),
+                        Color(0xFF03DAC5)
+                    )
+                )
+            )
+            .padding(bottom = 24.dp, top = 20.dp)
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = "Home Icon",
+                    tint = Color.White,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Spacer(Modifier.padding(4.dp))
+                Text(
+                    text = "Welcome in",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+                Text(
+                    text = "BakulApp!",
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+                Spacer(Modifier.height(4.dp))
+                Text(
+                    text = "Manage your inventory",
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+            }
+        }
+        Box(
+            Modifier
+                .align(Alignment.CenterEnd)
+                .padding(24.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.inventory),
+                contentDescription = "Warehouse Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+        }
+    }
+}
+
