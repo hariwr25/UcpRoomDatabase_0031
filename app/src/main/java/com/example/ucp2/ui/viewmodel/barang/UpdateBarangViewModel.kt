1a9b3847d378eb4f71ccd80e723f335fcf83572b
hariwr <hariwr25@gmail.com>
2024-12-25 01:57:37 +0700

    Inisialisasi UpdateBarangViewModel dan state default barang

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/UpdateBarangViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.repository.barang.RepositoryBrg
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateBarangViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel() {
+
+    var updateBrgUiState by mutableStateOf(brgUIState())
+        private set
+
+    private val _idBrg: String = checkNotNull(savedStateHandle[DestinasiUpdateBrg.idBrg])
+
+    init {
+        viewModelScope.launch {
+            updateBrgUiState = repositoryBrg.getBarang(_idBrg.toInt())
+                .filterNotNull()
+                .first()
+                .toUIStateBrg()
+        }
+    }
