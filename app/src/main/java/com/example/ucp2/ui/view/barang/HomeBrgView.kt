252e00a7b7e2758d06004f52ab22d9962c827e7b
hariwr <hariwr25@gmail.com>
2024-12-25 03:28:20 +0700

    Fix Code dan menambahkan fungsi cardbarang

--- app/src/main/java/com/example/ucp2/ui/view/barang/HomeBrgView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/HomeBrgView.kt
@@ -1,26 +1,38 @@
 package com.example.ucp2.ui.view.barang
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,7 +40,6 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.R
 import com.example.ucp2.data.entity.Barang
 import com.example.ucp2.ui.customwidget.LoadingState
-import com.example.ucp2.ui.customwidget.TopAppBar
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.ucp2.ui.viewmodel.barang.BarangHomeViewModel
 import com.example.ucp2.ui.viewmodel.barang.HomeUIStateBrg
@@ -75,6 +86,7 @@ fun HomeBrgView(
         )
     }
 }
+
 @Composable
 fun BodyHomeBrgView(
     homeUiState: HomeUIStateBrg,
@@ -124,6 +136,7 @@ fun BodyHomeBrgView(
         }
     }
 }
+
 @Composable
 fun ListBarang(
     listBrg: List<Barang>,
@@ -142,6 +155,96 @@ fun ListBarang(
                 )
             }
         )
+    }
+}
+
+@Composable
+fun CardBarang(
+    brg: Barang,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    val backgroundColor = when (brg.stok) {
+        0 -> Color(0xFFF3F4F6)
+        in 1..10 -> Color(0xFFFFCDD2)
+        else -> Color(0xFFC8E6C9)
+    }
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = backgroundColor
+        ),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.item),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp),
+                tint = Color(0xFF81D4FA)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = brg.namaBarang,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    color = Color(0xFF6200EA),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+                HorizontalDivider(color = Color.Gray.copy(alpha = 0.3f))
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "Deskripsi",
+                        modifier = Modifier.size(28.dp),
+                        tint = Color(0xFF81D4FA)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = brg.deskripsi,
+                        fontSize = 18.sp,
+                        color = Color(0xFF455A64),
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.money),
+                        contentDescription = "Harga",
+                        modifier = Modifier.size(28.dp),
+                        tint = Color(0xFF81D4FA)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "Harga : Rp." + brg.harga.toString(),
+                        fontSize = 18.sp,
+                        color = Color(0xFF455A64),
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
