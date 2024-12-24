e6651a84534600e7b8cd99e70afccceaa73ae6f2
hariwr <hariwr25@gmail.com>
2024-12-25 01:48:58 +0700

    Inisialisasi ViewModel dan implementasi StateFlow untuk mengelola state UI barang

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/DetailBarangViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.barang.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailBarangViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBrg: RepositoryBrg,
+) : ViewModel() {
+    private val _id : String = checkNotNull(savedStateHandle[DestinasiDetailBrg.idBrg])
+
+    val detailBrgUiState: StateFlow<DetailBrgUiState> = repositoryBrg.getBarang(_id.toInt())
+        .filterNotNull()
+        .map {
+            DetailBrgUiState(
+                detailUiBrgEvent = it.toDetailBrglUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailBrgUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(DetailBrgUiState(
+                isLoading = false,
+                isError = true,
+            ))
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailBrgUiState(
+                isLoading = true
+            )
+        )
+}
